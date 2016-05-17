f086f900e7d1761bc5773ae598989931596077c9
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-05-16 21:02:36 -0400

    Initial Commit

---
+++ COMP123S2016-LessonB1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Author: Chinatsu Kawakami
+ * Date: May 16th, 2016
+ * Date Modified: May 16th, 2016
+ * Student Id: 300821245
+ * Description: This program demonstrates GitHub and version control
+ * best practices. We also explore Unit Testing
+ * 
+ * Version: 0.1 - Initial Version
+ */
+namespace COMP123S2016_LessonB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
